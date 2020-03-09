8df7ae6d2def8d6c915750c389cf5992df547c1a
Hammarkvast <tomham-3@student.ltu.se>
2020-03-09 11:05:36 +0100

    Added example that runs on mcu

---
+++ radio-module/examples/hello.rs
@@ -0,0 +1,18 @@
+//! Prints "Hello, world!" on the host console using semihosting
+
+#![no_main]
+#![no_std]
+
+extern crate panic_semihosting;
+
+use cortex_m_rt::entry;
+use cortex_m_semihosting::hprintln;
+use stm32l0xx_hal as _;
+
+#[entry]
+fn main() -> ! {
+    hprintln!("Hello, world!").unwrap();
+    loop {
+        continue;
+    }
+}
